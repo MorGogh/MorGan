a5fefc55f3ff3be8a220041a5f5fa90baf64e3d1
laoyaoqiannian@gmail.com <laoyaoqiannian@gmail.com>
2024-08-23 02:06:26 +0800

    Print

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// 这是我第一个程序
+	fmt.Println("Hello.World\n" + "Hello.Morgan!")
+	var age = rand.Float64()
+	fmt.Println("他今年", age)
+
+}
