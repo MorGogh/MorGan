4c375f0ef6245c016c0ba70cb2c5cec6ee89a3c7
laoyaoqiannian@gmail.com <laoyaoqiannian@gmail.com>
2024-08-23 07:14:29 +0800

    varAConst

--- main.go
+++ main.go
@@ -6,9 +6,19 @@ import (
 )
 
 func main() {
-	// 这是我第一个程序
-	fmt.Println("Hello.World\n" + "Hello.Morgan!")
-	var age = rand.Float64()
-	fmt.Println("他今年", age)
+	fmt.Print("Hello,World")
+	// 变量的声明
+	var x int
+	var s string
+	var b bool
+	// 随机生成数据
+	var num = rand.Intn(12)
+	// 变量打印使用 基本数据类型当变量只声明未赋值，默认为零值
+	fmt.Println(x)
+	fmt.Println(s)
+	fmt.Println(b)
+	if _, err := fmt.Println(num); err != nil {
+		return
+	}
 
 }
